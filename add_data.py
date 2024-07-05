e88704d4d01256f2197482eb27b6747d073c90d9
kazimirovi4 <big.boy926@gmail.com>
2024-07-05 18:28:14 +0300

    edit and add files

--- add_data.py
+++ add_data.py
@@ -2,10 +2,6 @@ import sqlite3
 
 
 
-
-orders_insert = "INSERT OR IGNORE INTO Заказы (Клиент_id, Товар_арт, admin_id, Дата_создания, Статус) values(?, ?, ?, ?, ?)"
-categories_insert = "INSERT OR IGNORE INTO Категории (Имя) values(?)"
-
 def add_admin(login, password, level):
     conn = sqlite3.connect('crm.db')
     c = conn.cursor()
@@ -20,5 +16,14 @@ def add_order(Клиент_id, Товар_арт, admin_id, Статус):
     conn.commit()
     conn.close()
 
-add_admin('admin', 'admin', 'superuser')
-add_order(1, 'LP1001', 1, 'В_пути')
+def add_category(Имя):
+    conn = sqlite3.connect('crm.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO Категории (Имя) VALUES (?)", (Имя,))
+    conn.commit()
+    conn.close()
+
+
+# add_admin('admin', 'admin', 'superuser')
+# add_order(1, 'LP1001', 1, 'В_пути')
+add_category('Молочная продукция')
