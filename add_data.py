b4ca5edf971fe26f40344ffa0ce99446027de37a
kazimirovi4 <big.boy926@gmail.com>
2024-07-05 18:23:45 +0300

    edit and add files

--- add_data.py
+++ add_data.py
@@ -1,10 +1,24 @@
 import sqlite3
 
 
-conn = sqlite3.connect('crm.db')
-c = conn.cursor()
 
 
+orders_insert = "INSERT OR IGNORE INTO Заказы (Клиент_id, Товар_арт, admin_id, Дата_создания, Статус) values(?, ?, ?, ?, ?)"
+categories_insert = "INSERT OR IGNORE INTO Категории (Имя) values(?)"
 
-conn.commit()
-conn.close()
+def add_admin(login, password, level):
+    conn = sqlite3.connect('crm.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO admins (login, pass, level) VALUES (?, ?, ?)", (login, password, level))
+    conn.commit()
+    conn.close()
+
+def add_order(Клиент_id, Товар_арт, admin_id, Статус):
+    conn = sqlite3.connect('crm.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO Заказы (Клиент_id, Товар_арт, admin_id, Статус) VALUES (?, ?, ?, ?)", (Клиент_id, Товар_арт, admin_id, Статус))
+    conn.commit()
+    conn.close()
+
+add_admin('admin', 'admin', 'superuser')
+add_order(1, 'LP1001', 1, 'В_пути')
--- bd.py
+++ bd.py
@@ -7,13 +7,14 @@ conn = sqlite3.connect('crm.db')
 c = conn.cursor()
 
 c.execute('''CREATE TABLE IF NOT EXISTS admins
-                 (id INTEGER PRIMARY KEY AUTOINCREMENT, login VARCHAR , pass VARCHAR, level VARCHAR)''')
+                 (id INTEGER PRIMARY KEY AUTOINCREMENT, login VARCHAR UNIQUE, pass VARCHAR, level VARCHAR)''')
 
 c.execute('''CREATE TABLE IF NOT EXISTS Категории
                  (id INTEGER PRIMARY KEY AUTOINCREMENT, Имя VARCHAR)''')
 
 c.execute('''CREATE TABLE IF NOT EXISTS Товары
-                 (id INTEGER PRIMARY KEY AUTOINCREMENT, Имя VARCHAR, Артикул VARCHAR, Категория_id INTEGER, Характеристики VARCHAR, Картинка VARCHAR, 
+                 (id INTEGER PRIMARY KEY AUTOINCREMENT, Имя VARCHAR, Артикул VARCHAR UNIQUE, 
+                 Категория_id INTEGER, Характеристики VARCHAR, Картинка VARCHAR, 
                  FOREIGN KEY(Категория_id) REFERENCES Категории(id))''')
 
 c.execute('''CREATE TABLE IF NOT EXISTS Склады
@@ -23,15 +24,13 @@ c.execute('''CREATE TABLE IF NOT EXISTS Клиенты
                  (id INTEGER PRIMARY KEY AUTOINCREMENT, ФИО VARCHAR, Адрес VARCHAR, Телефон VARCHAR)''')
 
 c.execute('''CREATE TABLE IF NOT EXISTS Заказы
-                 (id INTEGER PRIMARY KEY AUTOINCREMENT, Клиент_id VARCHAR, Название VARCHAR, admin_id VARCHAR, Дата_создания DATETIME, Статус VARCHAR,
-                 FOREIGN KEY(Клиент_id) REFERENCES Клиенты(id), FOREIGN KEY(admin_id) REFERENCES admins(id))''')
+                 (id INTEGER PRIMARY KEY AUTOINCREMENT, Клиент_id VARCHAR, Товар_арт VARCHAR, admin_id VARCHAR, 
+                 Дата_создания DEFAULT CURRENT_DATE, Время_создания DEFAULT CURRENT_TIME, Статус VARCHAR, FOREIGN KEY(Клиент_id) REFERENCES Клиенты(id), 
+                 FOREIGN KEY(admin_id) REFERENCES admins(id), FOREIGN KEY(Товар_арт) REFERENCES Товары(Артикул))''')
 
 c.execute('''CREATE TABLE IF NOT EXISTS Документы
-                 (id INTEGER PRIMARY KEY AUTOINCREMENT, Название VARCHAR, Тип VARCHAR, Дата_создания DATETIME, Содержание VARCHAR)''')
-
-c.execute('''CREATE TABLE IF NOT EXISTS Товар_в_заказе
-                 (id INTEGER PRIMARY KEY AUTOINCREMENT, Заказ_id VARCHAR, Товар_id VARCHAR, Количество INTAGER,
-                 FOREIGN KEY(Заказ_id) REFERENCES Заказы(id), FOREIGN KEY(Товар_id) REFERENCES Товары(id))''')
+                 (id INTEGER PRIMARY KEY AUTOINCREMENT, Название VARCHAR, Тип VARCHAR, Дата_создания DATETIME, 
+                 Содержание VARCHAR)''')
 
 c.execute('''CREATE TABLE IF NOT EXISTS Товар_Склад
                  (id INTEGER PRIMARY KEY AUTOINCREMENT, Товар_id VARCHAR, Склад_id VARCHAR, Количество INTAGER,
