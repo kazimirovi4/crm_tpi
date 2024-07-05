eec1da6a46ddc8307f518b7ec03e79779f5296f8
Kanstantsin.Bahutski <kanstantsin.bahutski1@gmail.com>
2024-07-05 15:25:39 +0300

    я добавил что-то, посмотрим что из этого получится

---
+++ testMagMainWind.py
@@ -0,0 +1,55 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(1123, 867)
+        self.pushButton = QtWidgets.QPushButton(Dialog)
+        self.pushButton.setGeometry(QtCore.QRect(70, 50, 181, 71))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_2.setGeometry(QtCore.QRect(320, 50, 181, 71))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_3 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_3.setGeometry(QtCore.QRect(580, 50, 181, 71))
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_4 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_4.setGeometry(QtCore.QRect(840, 50, 181, 71))
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.lineEdit = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit.setGeometry(QtCore.QRect(70, 180, 961, 41))
+        self.lineEdit.setObjectName("lineEdit")
+        self.tableWidget = QtWidgets.QTableWidget(Dialog)
+        self.tableWidget.setGeometry(QtCore.QRect(70, 280, 961, 421))
+        self.tableWidget.setObjectName("tableWidget")
+        self.tableWidget.setColumnCount(6)
+        self.tableWidget.setRowCount(10)
+
+        self.tableWidget.setHorizontalHeaderLabels([
+            "Действие", "Клиент", "Ответственное лицо", "Дата транзакции", "Статус транзакции", "Комментарий"])
+
+        self.tableWidget.horizontalHeader().setStretchLastSection(True)
+        self.tableWidget.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.pushButton.setText(_translate("Dialog", "Добавить"))
+        self.pushButton_2.setText(_translate("Dialog", "Оформить покупку"))
+        self.pushButton_3.setText(_translate("Dialog", "Перемещение"))
+        self.pushButton_4.setText(_translate("Dialog", "Заглушка"))
+        self.lineEdit.setPlaceholderText(_translate("Dialog", "Поиск транзакций"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
---
+++ testmaglog.py
@@ -0,0 +1,78 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+import testMagMainWind
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(400, 300)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(80, 80, 55, 16))
+        self.label.setObjectName("label")
+
+        self.lineEdit_username = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_username.setGeometry(QtCore.QRect(150, 80, 150, 22))
+        self.lineEdit_username.setObjectName("lineEdit_username")
+
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(80, 120, 55, 16))
+        self.label_2.setObjectName("label_2")
+
+        self.lineEdit_password = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_password.setGeometry(QtCore.QRect(150, 120, 150, 22))
+        self.lineEdit_password.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.lineEdit_password.setObjectName("lineEdit_password")
+
+        self.pushButton_login = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_login.setGeometry(QtCore.QRect(150, 160, 93, 28))
+        self.pushButton_login.setObjectName("pushButton_login")
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 400, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Login Window"))
+        self.label.setText(_translate("MainWindow", "Login"))
+        self.label_2.setText(_translate("MainWindow", "Password"))
+        self.pushButton_login.setText(_translate("MainWindow", "Войти"))
+
+class LoginWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.pushButton_login.clicked.connect(self.check_login)
+
+    def check_login(self):
+        username = self.lineEdit_username.text()
+        password = self.lineEdit_password.text()
+
+        if username == "111" and password == "111":
+            self.open_main_window()
+        else:
+            QtWidgets.QMessageBox.critical(self, "Ошибка", "Неверный логин или пароль")
+
+    def open_main_window(self):
+        self.main_window = QtWidgets.QDialog()
+        self.ui = testMagMainWind.Ui_Dialog()
+        self.ui.setupUi(self.main_window)
+        self.main_window.show()
+        self.close()
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = LoginWindow()
+    MainWindow.show()
+    sys.exit(app.exec_())
