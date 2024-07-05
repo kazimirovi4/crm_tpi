c29b4cd0ae09d19719c5903ab69fc8335dbe99dc
kazimirovi4 <big.boy926@gmail.com>
2024-07-06 00:17:15 +0300

    edit and add files

--- add_data.py
+++ add_data.py
@@ -1,7 +1,6 @@
 import sqlite3
 
 
-
 def add_admin(login, password, level):
     conn = sqlite3.connect('crm.db')
     c = conn.cursor()
@@ -9,12 +8,15 @@ def add_admin(login, password, level):
     conn.commit()
     conn.close()
 
+
 def add_order(Клиент_id, Товар_арт, admin_id, Статус):
     conn = sqlite3.connect('crm.db')
     c = conn.cursor()
-    c.execute("INSERT INTO Заказы (Клиент_id, Товар_арт, admin_id, Статус) VALUES (?, ?, ?, ?)", (Клиент_id, Товар_арт, admin_id, Статус))
+    c.execute("INSERT INTO Заказы (Клиент_id, Товар_арт, admin_id, Статус) VALUES (?, ?, ?, ?)",
+              (Клиент_id, Товар_арт, admin_id, Статус))
     conn.commit()
     conn.close()
+
 
 def add_category(Имя):
     conn = sqlite3.connect('crm.db')
@@ -24,6 +26,20 @@ def add_category(Имя):
     conn.close()
 
 
+def add_product(Имя, Артикул, Категория_id, Характеристики, Картинка):
+    conn = sqlite3.connect('crm.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO Товары (Имя, Артикул, Категория_id, Характеристики, Картинка) VALUES(?, ?, ?, ?, ?)",
+              (Имя, Артикул, Категория_id, Характеристики, Картинка))
+    conn.commit()
+    conn.close()
+
+
+
+
+
 # add_admin('admin', 'admin', 'superuser')
 # add_order(1, 'LP1001', 1, 'В_пути')
-add_category('Молочная продукция')
+# add_category('Молочная продукция')
+# add_product('Молоко', 'LP1001', 1, 'Бутыла 1л', '!!!!!!!!!')
+
